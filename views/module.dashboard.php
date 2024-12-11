4fef239f2e0bac95ec8036cac3e91733c0fda937
statshakya <statshakya@gmail.com>
2024-12-11 16:32:51 +0545

    [fix]-2.create a job button in breadcrumb in client dashboard

--- views/module.dashboard.php
+++ views/module.dashboard.php
@@ -699,10 +699,15 @@ if (!empty($_SESSION)) {
             </div>
         </div>
         <div class="bg-dark-blue">
-            <div class="container">
-                <h1 class="text-light py-5 fw-light fs-1">
+            <div class="container py-5 d-flex align-items-center justify-content-between">
+                <h1 class="text-light fw-light fs-1">
                     Dashboard
                 </h1>
+                <a href="' .
+            BASE_URL .
+            'create-job" class="btn btn-dark bg-light text-dark px-4 py-2 fs-6 rounded-0">
+                Create Job
+                        </a>
             </div>
         </div>
         <section>
