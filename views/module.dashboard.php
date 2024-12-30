040044615a6b5fe4ada72b37b9f4585e16e3b196
Swarna-Shakya <swarnamanshakya@gmail.com>
2024-12-30 11:37:08 +0545

    [EDIT] hide edit button if job is timeout

--- views/module.dashboard.php
+++ views/module.dashboard.php
@@ -902,20 +902,27 @@ if (!empty($_SESSION)) {
         if (!empty($Records)) {
             // pr($Records);
             foreach ($Records as $record) {
-                $jobdatas= jobs::find_by_id($record->job_id);
+                $jobdatas = jobs::find_by_id($record->job_id);
 
                 $totalbids = bids::find_total_bids($record->job_id);
                 $jobstatus = "";
                 switch ($record->project_status) {
                     case 1:
+                        $edit_bid_txt = '';
                         $encode_id = base64_encode($record->id);
+                        // do not show bid edit button if job is timeout
+                        if($jobdatas->project_status != 4) {
+                            $edit_bid_txt .= '
+                                <a href="' . BASE_URL . 'edit-bid/' . $encode_id . '" class="btn btn-outline-success bg-success-subtle text-success fs-7 rounded-0 px-3 py-1">
+                                    Edit Bid
+                                </a>
+                            ';
+                        }
                         $jobstatus = '<div class="col-12 col-md-2 d-flex align-items-start flex-column">
                             <p class="text-dark-blue fs-6 fw-bold">
                                 Bid On Progress
                             </p>
-                            <a href="' . BASE_URL . 'edit-bid/' . $encode_id . '" class="btn btn-outline-success bg-success-subtle text-success fs-7 rounded-0 px-3 py-1">
-                                Edit Bid
-                            </a>
+                            ' . $edit_bid_txt . '
                             </div>';
                         // if ($totalbids > 0) {
                         //     $jobstatus .= '<div class="d-inline-block bg-dark-subtle px-3 view-select">
